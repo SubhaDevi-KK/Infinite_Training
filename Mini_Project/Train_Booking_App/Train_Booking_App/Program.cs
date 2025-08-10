923991d74592a5c5314e7484adbc73699002ffc0
Subhadevi Karthikeyan <subhadevik@infinite.com>
2025-08-10 15:50:56 +0530

    mini project

---
+++ Mini_Project/Train_Booking_App/Train_Booking_App/Program.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static string connectionString = "Data Source=ICS-LT-4C2BJ84\\SQLEXPRESS;Initial Catalog=assignments;User ID=sa;Password=Infinite@1234;";
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("---------- Railway Reservation System ----------");
+            Console.WriteLine("1. Book Ticket");
+            Console.WriteLine("2. Check Ticket Status");
+            Console.WriteLine("3. Cancel Booking");
+            Console.WriteLine("4. Exit");
+            Console.Write("Choose an option: ");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+                BookTicket();
+            else if (choice == "2")
+                CheckStatus();
+            else if (choice == "3")
+                CancelBooking();
+            else if (choice == "4")
+                break;
+            else
+                Console.WriteLine("Invalid choice.");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+
+    static void BookTicket()
+    {
+        Console.Write("Enter Source: ");
+        string source = Console.ReadLine();
+        Console.Write("Enter Destination: ");
+        string destination = Console.ReadLine();
+        Console.Write("Enter Journey Date (yyyy-mm-dd): ");
+        string journeyDate = Console.ReadLine();
+
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            con.Open();
+
+            using (SqlCommand cmdTrain = new SqlCommand(
+                "SELECT trainid, trainname FROM train WHERE source=@src AND destination=@dest AND dateofjourney=@date", con))
+            {
+                cmdTrain.Parameters.AddWithValue("@src", source);
+                cmdTrain.Parameters.AddWithValue("@dest", destination);
+                cmdTrain.Parameters.AddWithValue("@date", journeyDate);
+
+                using (SqlDataReader reader = cmdTrain.ExecuteReader())
+                {
+                    Console.WriteLine("\nAvailable Trains:");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["trainid"]} --> {reader["trainname"]}");  //retrieving data
+                    }
+                }
+            }
+
+            Console.Write("Choose Train ID: ");
+            int trainId = int.Parse(Console.ReadLine());
+
+            using (SqlCommand cmdCompartments = new SqlCommand(
+                "SELECT compartmentid, compartmenttype, seatprice, availableseats FROM train_compartments WHERE trainid=@id", con))
+            {
+                cmdCompartments.Parameters.AddWithValue("@id", trainId);
+                using (SqlDataReader compReader = cmdCompartments.ExecuteReader())
+                {
+                    Console.WriteLine("\nAvailable Compartments:");
+                    while (compReader.Read())
+                    {
+                        Console.WriteLine($"{compReader["compartmentid"]}: {compReader["compartmenttype"]} - ₹{compReader["seatprice"]} - Seats Left: {compReader["availableseats"]}");
+                    }
+                }
+            }
+
+            Console.Write("Select Compartment ID: ");
+            int compartmentId = int.Parse(Console.ReadLine());
+            Console.Write("Number of Seats: ");
+            int numSeats = int.Parse(Console.ReadLine());
+
+            int available = 0;  //To store the number of available seats in the selected train compartment.
+            decimal price = 0;  //To store the price of one seat in the selected compartment.
+
+            using (SqlCommand checkSeats = new SqlCommand("SELECT availableseats, seatprice FROM train_compartments WHERE compartmentid=@cid", con))
+            {
+                checkSeats.Parameters.AddWithValue("@cid", compartmentId);
+                using (SqlDataReader seatReader = checkSeats.ExecuteReader())
+                {
+                    if (!seatReader.Read()) return;
+                    available = (int)seatReader["availableseats"];
+                    price = (decimal)seatReader["seatprice"];
+                }
+            }
+
+            if (numSeats > available)
+            {
+                Console.WriteLine("Sorry, not enough seats available.");
+                return;
+            }
+
+            string status = "Confirmed";
+            string[] seatTypes = { "Upper", "Middle", "Lower" };
+
+            for (int i = 1; i <= numSeats; i++)
+            {
+                Console.WriteLine($"\nEnter details for Passenger {i}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Age: ");
+                int age = int.Parse(Console.ReadLine());
+                Console.Write("Phone: ");
+                string phone = Console.ReadLine();
+                Console.Write("Address: ");
+                string address = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Aadhar: ");
+                string aadhar = Console.ReadLine();
+
+                string seatPosition = seatTypes[(i - 1) % seatTypes.Length];  //ensures seat types are assigned in a rotating pattern.
+                decimal total = price;   // fare for one passenger.
+
+                using (SqlCommand insert = new SqlCommand(
+                    "INSERT INTO booking (trainid, compartmentid, name, age, phone, address, email, aadhar, numseats, totalprice, seatposition, status) " +
+                    "VALUES (@trainid, @compid, @name, @age, @phone, @addr, @email, @aadhaar, @num, @total, @seat, @status); " +
+                    "SELECT CAST(SCOPE_IDENTITY() AS INT);", con))
+                {
+                    insert.Parameters.AddWithValue("@trainid", trainId);
+                    insert.Parameters.AddWithValue("@compid", compartmentId);
+                    insert.Parameters.AddWithValue("@name", name);
+                    insert.Parameters.AddWithValue("@age", age);
+                    insert.Parameters.AddWithValue("@phone", phone);
+                    insert.Parameters.AddWithValue("@addr", address);
+                    insert.Parameters.AddWithValue("@email", email);
+                    insert.Parameters.AddWithValue("@aadhaar", aadhar);
+                    insert.Parameters.AddWithValue("@num", 1);
+                    insert.Parameters.AddWithValue("@total", total);
+                    insert.Parameters.AddWithValue("@seat", seatPosition);
+                    insert.Parameters.AddWithValue("@status", status);
+
+                    int bookingId = (int)insert.ExecuteScalar();
+
+                    Console.WriteLine($"\n Passenger {i} Booked Successfully!");
+                    Console.WriteLine($" Passenger {i} — Booking ID: {bookingId}");
+                    Console.WriteLine($" Seat Type: {seatPosition}");
+                    Console.WriteLine($" Fare: ₹{total}");
+                }
+            }
+
+            using (SqlCommand updateSeats = new SqlCommand(
+                "UPDATE train_compartments SET availableseats = availableseats - @n WHERE compartmentid = @cid", con))
+            {
+                updateSeats.Parameters.AddWithValue("@n", numSeats);
+                updateSeats.Parameters.AddWithValue("@cid", compartmentId);
+                updateSeats.ExecuteNonQuery();
+            }
+
+            Console.WriteLine("\nAll passengers booked! Don’t forget to save each Booking ID for status check or cancellation.");
+        }
+    }
+
+    static void CheckStatus()
+    {
+        Console.Write("Enter Booking ID: ");
+        int bookingId = int.Parse(Console.ReadLine());
+
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            con.Open();
+
+            string query = @"SELECT b.status, b.name, b.age, b.phone, b.email, b.seatposition, 
+                        t.trainname, t.source, t.destination, t.dateofjourney
+                 FROM booking b
+                 JOIN train t ON b.trainid = t.trainid
+                 WHERE b.bookingid = @id AND b.isdeleted = 0";
+
+
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                cmd.Parameters.AddWithValue("@id", bookingId);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        Console.WriteLine("\n Ticket Details:");
+                        Console.WriteLine($" Name: {reader["name"]}, Age: {reader["age"]}");
+                        Console.WriteLine($" Phone: {reader["phone"]},  Email: {reader["email"]}");
+                        Console.WriteLine($" Seat Type: {reader["seatposition"]}");
+                        Console.WriteLine($" Train: {reader["trainname"]}");
+                        Console.WriteLine($" Route: {reader["source"]}  {reader["destination"]}");
+                        Console.WriteLine($"Date of Journey: {Convert.ToDateTime(reader["dateofjourney"]).ToShortDateString()}");
+                        Console.WriteLine($" Booking Status: {reader["status"]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" No booking found for the given ID.");
+                    }
+                }
+            }
+        }
+    }
+
+    static void CancelBooking()
+    {
+        Console.Write("Enter Booking ID to cancel: ");
+        int bookingId = int.Parse(Console.ReadLine());
+
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            con.Open();
+            decimal total = 0;
+            string status = "";
+
+            using (SqlCommand fetch = new SqlCommand("SELECT totalprice, status FROM booking WHERE bookingid=@id", con))
+            {
+                fetch.Parameters.AddWithValue("@id", bookingId);
+                using (SqlDataReader reader = fetch.ExecuteReader())
+                {
+                    if (!reader.Read()) { Console.WriteLine("Invalid Booking ID."); return; }
+                    total = (decimal)reader["totalprice"];
+                    status = (string)reader["status"];
+                }
+            }
+
+            if (status == "Cancelled")
+            {
+                Console.WriteLine("Already cancelled.");
+                return;
+            }
+
+            Console.Write("Confirm cancellation (y/n): ");
+            string confirm = Console.ReadLine();
+
+            if (confirm.ToLower() != "y")
+            {
+                Console.WriteLine("Cancellation aborted.");
+                return;
+            }
+
+            decimal refund = total * 0.5m;   //multiplies total fare by 50% so user get half amount back refund
+
+            using (SqlCommand cancel = new SqlCommand(
+                "INSERT INTO cancellation (bookingid, refundamount, canceldate) VALUES (@id, @refund, GETDATE())", con))
+            {
+                cancel.Parameters.AddWithValue("@id", bookingId);
+                cancel.Parameters.AddWithValue("@refund", refund);
+                cancel.ExecuteNonQuery();
+            }
+
+            using (SqlCommand update = new SqlCommand(
+      "UPDATE booking SET status='Cancelled', isdeleted=1 WHERE bookingid=@id", con))//status='Cancelled' updates the booking status to show it's no longer active.
+            {//isdeleted=1 indicates it is logically deleted but still it exist in booking table flagging it as deleted
+                update.Parameters.AddWithValue("@id", bookingId);
+                update.ExecuteNonQuery();
+            }
+
+
+            Console.WriteLine($"Booking cancelled successfully.");
+            Console.WriteLine($" Refund Amount: ₹{refund} has been initiated.");
+        }
+    }
+}
---
+++ Mini_Project/Train_Booking_App/data.sql
@@ -0,0 +1,166 @@
+-- Train table
+CREATE TABLE train (
+    trainid INT PRIMARY KEY,
+    trainname VARCHAR(100),
+    source VARCHAR(100),
+    destination VARCHAR(100),
+    dateofjourney DATE,
+    totalseats INT,
+    availableseats INT
+)
+
+-- Compartment table
+CREATE TABLE train_compartments (
+    compartmentid INT PRIMARY KEY IDENTITY(1,1),
+    trainid INT FOREIGN KEY REFERENCES train(trainid),
+    compartmenttype VARCHAR(50), -- Sleeper, 1AC, 2AC, 3AC, Ladies Sleeper, Disabled Sleeper
+    seatprice DECIMAL(10,2),
+    availableseats INT
+)
+
+-- Booking table
+CREATE TABLE booking (
+    bookingid INT PRIMARY KEY IDENTITY(1000,1),
+    trainid INT FOREIGN KEY REFERENCES train(trainid),
+    compartmentid INT FOREIGN KEY REFERENCES train_compartments(compartmentid),
+    name VARCHAR(100),
+    age INT,
+    phone VARCHAR(15),
+    address VARCHAR(255),
+    email VARCHAR(100),
+    aadhar VARCHAR(20),
+    numseats INT,
+    totalprice DECIMAL(10,2),
+    seatposition VARCHAR(50), -- Upper, Middle, Lower
+    status VARCHAR(50) -- Confirmed / Waiting List / Cancelled
+)
+
+-- Cancellation table: Tracks refunds
+CREATE TABLE cancellation (
+    cancelid INT PRIMARY KEY IDENTITY(1,1),
+    bookingid INT FOREIGN KEY REFERENCES booking(bookingid),
+    refundamount DECIMAL(10,2),
+    canceldate DATE
+)
+INSERT INTO train (trainid, trainname, source, destination, dateofjourney, totalseats, availableseats) VALUES
+(101, 'Rajdhani Express', 'Delhi', 'Mumbai', '2025-08-05', 300, 140),
+(102, 'Shatabdi Express', 'Delhi', 'Chandigarh', '2025-08-06', 200, 110),
+(103, 'Duronto Express', 'Kolkata', 'Pune', '2025-08-06', 280, 90),
+(104, 'Garib Rath', 'Bangalore', 'Hyderabad', '2025-08-06', 250, 150),
+(105, 'Intercity Express', 'Lucknow', 'Delhi', '2025-08-06', 220, 115),
+(106, 'Tejas Express', 'Chennai', 'Bangalore', '2025-08-08', 230, 160),
+(107, 'Howrah Mail', 'Howrah', 'Chennai', '2025-08-09', 260, 120),
+(108, 'Udyan Express', 'Mumbai', 'Bangalore', '2025-08-10', 240, 130),
+(109, 'Kalka Shatabdi', 'Delhi', 'Kalka', '2025-08-08', 200, 90),
+(110, 'Netravati Express', 'Thiruvananthapuram', 'Mumbai', '2025-08-11', 280, 140),
+(111, 'Himalayan Queen', 'Delhi', 'Shimla', '2025-08-09', 180, 80),
+(112, 'Goa Express', 'Delhi', 'Goa', '2025-08-10', 230, 100),
+(113, 'Deccan Queen', 'Pune', 'Mumbai', '2025-08-08', 210, 115)
+
+
+-- For TrainID 101: Rajdhani Express
+INSERT INTO train_compartments VALUES
+(101, 'Sleeper', 600.00, 50),
+(101, '3AC', 1200.00, 40),
+(101, '2AC', 1800.00, 30),
+(101, '1AC', 2500.00, 20),
+(101, 'Ladies Sleeper', 600.00, 10),
+(101, 'Disabled Sleeper', 600.00, 10)
+
+-- For TrainID 102: Shatabdi Express
+INSERT INTO train_compartments VALUES
+(102, 'Chair Car', 550.00, 60),
+(102, 'Executive Chair Car', 1050.00, 50),
+(102, 'Ladies Chair Car', 600.00, 20)
+
+-- TrainID 103: Duronto Express
+INSERT INTO train_compartments VALUES
+(103, 'Sleeper', 650.00, 60),
+(103, '3AC', 1150.00, 40),
+(103, '2AC', 1750.00, 30),
+(103, '1AC', 2400.00, 20),
+(103, 'Ladies Sleeper', 650.00, 10),
+(103, 'Disabled Sleeper', 650.00, 10)
+
+-- TrainID 104: Garib Rath 
+INSERT INTO train_compartments VALUES
+(104, 'Sleeper', 500.00, 60),
+(104, '3AC', 850.00, 90),
+(104, 'Ladies 3AC', 850.00, 20),
+(104, 'Disabled Sleeper', 500.00, 10)
+
+-- TrainID 105: Intercity Express
+INSERT INTO train_compartments VALUES
+(105, 'Sleeper', 400.00, 50),
+(105, '3AC', 700.00, 40),
+(105, '2AC', 1200.00, 30),
+(105, 'Ladies Sleeper', 400.00, 10),
+(105, 'Disabled Sleeper', 400.00, 10)
+
+-- TrainID 106: Tejas Express 
+INSERT INTO train_compartments VALUES
+(106, 'Executive Class', 2100.00, 60),
+(106, 'AC Chair Car', 1400.00, 100),
+(106, 'Ladies AC Chair Car', 1450.00, 30),
+(106, 'Disabled Chair Car', 1400.00, 10)
+
+-- TrainID 107: Howrah Mail
+INSERT INTO train_compartments VALUES
+(107, 'Sleeper', 550.00, 60),
+(107, '3AC', 1100.00, 40),
+(107, '2AC', 1600.00, 30),
+(107, 'Ladies Sleeper', 550.00, 10),
+(107, 'Disabled Sleeper', 550.00, 10)
+
+-- TrainID 108: Udyan Express
+INSERT INTO train_compartments VALUES
+(108, 'Sleeper', 600.00, 60),
+(108, '3AC', 1200.00, 40),
+(108, '2AC', 1800.00, 30),
+(108, '1AC', 2500.00, 20),
+(108, 'Ladies Sleeper', 600.00, 10),
+(108, 'Disabled Sleeper', 600.00, 10)
+
+-- TrainID 109: Kalka Shatabdi
+INSERT INTO train_compartments VALUES
+(109, 'Chair Car', 500.00, 60),
+(109, 'Executive Chair Car', 1000.00, 40),
+(109, 'Ladies Chair Car', 550.00, 10),
+(109, 'Disabled Chair Car', 500.00, 10)
+
+-- TrainID 110: Netravati Express
+INSERT INTO train_compartments VALUES
+(110, 'Sleeper', 620.00, 60),
+(110, '3AC', 1250.00, 40),
+(110, '2AC', 1850.00, 30),
+(110, 'Ladies Sleeper', 620.00, 10),
+(110, 'Disabled Sleeper', 620.00, 10)
+
+-- TrainID 111: Himalayan Queen
+INSERT INTO train_compartments VALUES
+(111, 'Sleeper', 550.00, 40),
+(111, '3AC', 950.00, 30),
+(111, 'Ladies Sleeper', 550.00, 10),
+(111, 'Disabled Sleeper', 550.00, 10)
+
+-- TrainID 112: Goa Express
+INSERT INTO train_compartments VALUES
+(112, 'Sleeper', 600.00, 50),
+(112, '3AC', 1100.00, 40),
+(112, '2AC', 1600.00, 30),
+(112, 'Ladies Sleeper', 600.00, 10),
+(112, 'Disabled Sleeper', 600.00, 10)
+
+-- TrainID 113: Deccan Queen
+INSERT INTO train_compartments VALUES
+(113, 'Chair Car', 650.00, 60),
+(113, 'Executive Chair Car', 1150.00, 40),
+(113, 'Ladies Chair Car', 650.00, 10),
+(113, 'Disabled Chair Car', 650.00, 10)
+
+select * from booking
+
+ALTER TABLE booking ADD isdeleted BIT NOT NULL DEFAULT 0
+
+
+
